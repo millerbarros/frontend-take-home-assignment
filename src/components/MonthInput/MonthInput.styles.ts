b76e5563ef914f1d0706f7d4c7e5e668cb02f8da
Miller Barros <millercbarros@gmail.com>
2021-03-14 21:38:32 -0300

    feat: create MonthInput component

---
+++ src/components/MonthInput/MonthInput.styles.ts
@@ -0,0 +1,100 @@
+import styled, { css } from 'styled-components';
+
+// STYLES
+import { toREM } from '~/styles/functions';
+import { breakpointMin } from '~/styles/mixins';
+import { COLOR, SPACING } from '~/styles/variables';
+
+interface ContainerProps {
+  $focussed: boolean;
+}
+
+interface ControllerProps {
+  $disabled?: boolean;
+}
+
+export const StyledHiddenInput = styled.input`
+  width: 0;
+  height: 0;
+  opacity: 0;
+  padding: 0;
+  margin: 0;
+  outline: 0;
+  border: none;
+`;
+
+export const StyledMonthInputContainer = styled.div<ContainerProps>`
+  display: flex;
+  border-radius: ${SPACING['2xs']};
+  border-style: solid;
+  border-width: 1px;
+  border-color: ${props =>
+    props.$focussed ? COLOR.blueGray300 : COLOR.blueGray50};
+  background-color: ${COLOR.neutralWhite};
+  height: ${toREM('56px')};
+`;
+
+export const StyledMonthInputContent = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0 ${SPACING.md};
+`;
+
+export const StyledMonthInputController = styled.button<ControllerProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: ${toREM('48px')};
+  height: 100%;
+  padding: 0;
+  border: none;
+  background: transparent;
+  outline: none;
+  color: ${props => (props.$disabled ? COLOR.blueGray50 : COLOR.blueGray300)};
+  cursor: ${props => (props.$disabled ? 'default' : 'pointer')};
+
+  svg {
+    width: ${toREM('8px')};
+  }
+
+  ${props => {
+    if (props.$disabled) return null;
+
+    return css`
+      &:hover {
+        color: ${COLOR.blueGray600};
+      }
+    `;
+  }}
+
+  ${breakpointMin.lg`
+    font-size: ${toREM('16px')};
+    line-height: ${toREM('24px')};
+  `}
+`;
+
+export const StyledMonthInputMonth = styled.div`
+  color: ${COLOR.blueGray900};
+  font-size: ${toREM('14px')};
+  line-height: ${toREM('21px')};
+  font-weight: bold;
+
+  ${breakpointMin.lg`
+    font-size: ${toREM('16px')};
+    line-height: ${toREM('24px')};
+  `}
+`;
+
+export const StyledMonthInputYear = styled.div`
+  color: ${COLOR.blueGray300};
+  font-size: ${toREM('14px')};
+  line-height: ${toREM('21px')};
+
+  ${breakpointMin.lg`
+    font-size: ${toREM('16px')};
+    line-height: ${toREM('24px')};
+  `}
+`;
---
+++ src/components/MonthInput/MonthInput.tsx
@@ -0,0 +1,166 @@
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState
+} from 'react';
+
+// HELPERS
+import {
+  formatMonth,
+  formatYear,
+  increaseDateMonth,
+  decreaseDateMonth,
+  isFutureMonth,
+  isCurrentMonth
+} from '~/helpers/date';
+
+// STYLES
+import {
+  StyledHiddenInput,
+  StyledMonthInputContainer,
+  StyledMonthInputContent,
+  StyledMonthInputController,
+  StyledMonthInputMonth,
+  StyledMonthInputYear
+} from './MonthInput.styles';
+
+// ASSETS
+import AngleLeftIcon from '~/assets/icons/angle-left.svg';
+import AngleRightIcon from '~/assets/icons/angle-right.svg';
+
+interface Props {
+  value?: string;
+  onChange?: (value: string) => void;
+}
+
+export const MonthInput = ({ value, onChange }: Props) => {
+  const [isoDate, setIsoDate] = useState(new Date().toISOString());
+  const [focussed, setFocus] = useState(false);
+  const [focusing, setFocusing] = useState(false);
+
+  const hiddenInputRef = useRef<HTMLInputElement>();
+
+  const prevMonth = useMemo(() => decreaseDateMonth(isoDate), [isoDate]);
+  const nextMonth = useMemo(() => increaseDateMonth(isoDate), [isoDate]);
+  const formatedMonth = useMemo(() => formatMonth(isoDate), [isoDate]);
+  const formatedYear = useMemo(() => formatYear(isoDate), [isoDate]);
+  const canDecrease = useMemo(() => {
+    return isFutureMonth(isoDate) && !isCurrentMonth(prevMonth);
+  }, [isoDate, prevMonth]);
+
+  const increaseMonth = useCallback(() => {
+    const newIsoDate = nextMonth.toISOString();
+
+    setIsoDate(newIsoDate);
+    if (onChange) onChange(newIsoDate);
+  }, [nextMonth, onChange]);
+
+  const decreaseMonth = useCallback(() => {
+    if (!canDecrease) return;
+
+    const newIsoDate = prevMonth.toISOString();
+
+    setIsoDate(newIsoDate);
+    if (onChange) onChange(newIsoDate);
+  }, [canDecrease, prevMonth, onChange]);
+
+  const applyFocus = () => {
+    hiddenInputRef.current?.focus();
+  };
+
+  // Handle value props changes
+  useEffect(() => {
+    if (!value || !isFutureMonth(value)) {
+      const date = new Date();
+      const nextMonth = date.getMonth() + 1;
+
+      date.setMonth(nextMonth);
+
+      const newIsoDate = date.toISOString();
+
+      setIsoDate(newIsoDate);
+      if (onChange) onChange(newIsoDate);
+
+      return;
+    }
+
+    setIsoDate(value);
+  }, [value, onChange]);
+
+  // Create left and right arrows keypress listeners on focus
+  useEffect(() => {
+    if (!focussed) return;
+
+    const inputElement = hiddenInputRef.current;
+
+    if (!inputElement) return;
+
+    const keypressListener = (e: KeyboardEvent) => {
+      if (!e.isTrusted || e.ctrlKey || e.altKey) return;
+
+      let key = e.key;
+
+      if (!key) {
+        const keyCode = e.which || e.keyCode;
+
+        if (keyCode === 37) {
+          key = 'ArrowLeft';
+        }
+
+        if (keyCode === 39) {
+          key = 'ArrowRight';
+        }
+      }
+
+      if (key === 'ArrowLeft') return decreaseMonth();
+      if (key === 'ArrowRight') return increaseMonth();
+    };
+
+    inputElement.addEventListener('keyup', keypressListener, true);
+
+    return () => {
+      inputElement.removeEventListener('keyup', keypressListener, true);
+    };
+  }, [focussed, increaseMonth, decreaseMonth]);
+
+  return (
+    <StyledMonthInputContainer
+      $focussed={focussed}
+      onMouseDown={() => setFocusing(true)}
+      onMouseUp={() => {
+        setFocusing(false);
+        applyFocus();
+      }}
+    >
+      <StyledHiddenInput
+        ref={hiddenInputRef}
+        onFocus={() => setFocus(true)}
+        onBlur={() => !focusing && setFocus(false)}
+      />
+
+      <StyledMonthInputController
+        type="button"
+        onClick={decreaseMonth}
+        $disabled={!canDecrease}
+        tabIndex={-1}
+      >
+        <AngleLeftIcon />
+      </StyledMonthInputController>
+
+      <StyledMonthInputContent>
+        <StyledMonthInputMonth>{formatedMonth}</StyledMonthInputMonth>
+        <StyledMonthInputYear>{formatedYear}</StyledMonthInputYear>
+      </StyledMonthInputContent>
+
+      <StyledMonthInputController
+        type="button"
+        onClick={increaseMonth}
+        tabIndex={-1}
+      >
+        <AngleRightIcon />
+      </StyledMonthInputController>
+    </StyledMonthInputContainer>
+  );
+};
